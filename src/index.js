59f5e80fbe9d25ae79c5b23d29e0147ff829b5db
veer260 <57021124+veer260@users.noreply.github.com>
2023-01-03 16:46:20 +0530

    Added basic express setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/server-config'); 
+
+const setupAndStartServer = () => {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port: ${PORT}`);
+    })
+
+}
+
+setupAndStartServer();
